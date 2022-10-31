cdc313b469e586a8a229baed3e3a8fcba06121b0
Osama saleem <osamasaleem515@gmail.com>
2022-10-31 18:45:10 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import time
+
+if 'photo' not in st.session_state:
+    st.session_state['photo']="not done"
+
+col1, col2, col3 = st.columns([1,2,1])
+
+col1.markdown('# Welcome to my Photo Upload app!')
+col1.markdown('Here is some info on the app.')
+
+def change_photo_state():
+    st.session_state['photo']='done'
+
+uploaded_photo = col2.file_uploader("Upload a photo", on_change=change_photo_state)
+camera_photo = col2.camera_input("Take a photo", on_change=change_photo_state)
+
+if st.session_state['photo']=='done':
+    progress_bar = col2.progress(0)
+
+    for perc_completed in range(100):
+        time.sleep(0.05)
+        progress_bar.progress(perc_completed+1)
+
+    col2.success('Photo uploaded successfully!')
+
+    col3.metric(label='Temperature', value='40 °C', delta='3 °C')
+
+    with st.expander('Click to see the uploaded image with message'):
+        st.write('Hello, here is your image.')
+
+        if uploaded_photo is None:
+            st.image(camera_photo)
+        else:
+            st.image(uploaded_photo)
